bae4a6d7d7f626d37c19679a58312d3eadc5d83e
mushan <mushan0x0@gmail.com>
2021-04-12 10:48:32 +0800

    fix: 富文本不同端表现不一致

--- src/rich-text/useProps.tsx
+++ src/rich-text/useProps.tsx
@@ -1,10 +1,10 @@
 import { RichTextProps } from 'remax/wechat';
 import classNames from 'classnames';
-import styles from '@/rich-text/index.less';
+import styles from './index.less';
 
 export default ({
   nodes = '',
-  space = 'ensp',
+  space = 'nbsp',
   className,
   ...props
 }: RichTextProps) => {
@@ -12,13 +12,9 @@ export default ({
     ...props,
     className: classNames(className, styles.text),
     space,
-    nodes:
-      nodes +
-      ''
-        ?.replace(/\.(png)|(jpg)/gi, `.$1?x-oss-process=image/resize,w_${1080}`)
-        ?.replace(
-          /<img/,
-          '<img style="width: 100%;margin-top: 20px;margin-bottom: 20px;"',
-        ),
+    nodes: (nodes + '')
+      ?.replace(/\.(png)|(jpg)/gi, `.$1?x-oss-process=image/resize,w_${1080}`)
+      ?.replace(/<img/g, '<img style="width: 100%"')
+      ?.replace(/<p/g, '<p style="margin-top: 10px;margin-bottom: 10px;"'),
   };
 };
