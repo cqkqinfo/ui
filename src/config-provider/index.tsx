2530084a7bbfa9fa4ffc255a469013a798e168ea
mushan <mushan0x0@gmail.com>
2021-04-14 16:11:08 +0800

    feat: 主题支持配置阴影组件的颜色

--- src/config-provider/index.tsx
+++ src/config-provider/index.tsx
@@ -4,9 +4,16 @@ export interface GlobalConfig {
   /**
    * 主题色
    */
-  brandPrimary: string;
+  brandPrimary?: string;
+  /**
+   * 阴影组件的颜色
+   */
+  shadowColor?: string;
 }
 
 export default createContainer<GlobalConfig, GlobalConfig>(
-  ({ brandPrimary = '#2780d9' } = {} as GlobalConfig) => ({ brandPrimary }),
+  ({ brandPrimary = '#2780d9', ...config } = {} as GlobalConfig) => ({
+    brandPrimary,
+    ...config,
+  }),
 );
--- src/shadow/index.tsx
+++ src/shadow/index.tsx
@@ -9,14 +9,14 @@ export interface Props {
 }
 
 export default ({ children, ...props }: Props) => {
-  const { brandPrimary } = provider.useContainer();
+  const { brandPrimary, shadowColor = brandPrimary } = provider.useContainer();
   return React.cloneElement(children, {
     ...children.props,
     ...props,
     style: {
       ...children.props.style,
       ...props.style,
-      boxShadow: `2px 2px 20px rgba(${new ColorRNA(brandPrimary)
+      boxShadow: `2px 2px 20px rgba(${new ColorRNA(shadowColor)
         .rgb()
         .join(',')}, 0.15)`,
     },
