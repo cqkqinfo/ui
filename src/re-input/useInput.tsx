b9205cbb1ecba549237d57fb59b00f761915fd76
mushan <mushan0x0@gmail.com>
2021-05-19 14:36:39 +0800

    fix: 微信环境输入

--- src/re-input/useInput.tsx
+++ src/re-input/useInput.tsx
@@ -37,8 +37,11 @@ export default ({
     },
     onInput: useCallback(
       (e: any) => {
-        const value = e.target.value;
+        const value = e.detail?.value || e.target.value;
         onChange?.(value);
+        return {
+          value,
+        };
       },
       [onChange],
     ),
