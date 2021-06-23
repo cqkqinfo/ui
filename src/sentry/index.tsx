2d6d0a1525f3e292765a9ffd22f08b26be9c007b
mushan <mushan0x0@gmail.com>
2021-06-23 11:25:56 +0800

    chore: 本地调试不监控错误

--- src/sentry/index.tsx
+++ src/sentry/index.tsx
@@ -3,36 +3,38 @@ import Sentry from './sentry';
 const newSentry: typeof Sentry = {
   ...Sentry,
   init: ({ beforeSend, ...options } = {}) => {
-    Sentry.init({
-      dsn: 'https://2306075284d9444894a37d888ef6977a@sentry.parsec.com.cn/13',
-      integrations: [new Sentry.Integrations.GlobalHandlers()],
-      beforeSend(event, hint) {
-        /* tslint:disable:no-string-literal only-arrow-functions */
-        const isNonErrorException =
-          event.exception?.values?.[0]?.value?.startsWith(
-            'Non-Error exception captured',
-          ) ||
-          (hint?.originalException as any)?.message?.startsWith(
-            'Non-Error exception captured',
-          );
-        /* tslint:enable:no-string-literal only-arrow-functions */
+    if (!window.location.host.includes('localhost')) {
+      Sentry.init({
+        dsn: 'https://2306075284d9444894a37d888ef6977a@sentry.parsec.com.cn/13',
+        integrations: [new Sentry.Integrations.GlobalHandlers()],
+        beforeSend(event, hint) {
+          /* tslint:disable:no-string-literal only-arrow-functions */
+          const isNonErrorException =
+            event.exception?.values?.[0]?.value?.startsWith(
+              'Non-Error exception captured',
+            ) ||
+            (hint?.originalException as any)?.message?.startsWith(
+              'Non-Error exception captured',
+            );
+          /* tslint:enable:no-string-literal only-arrow-functions */
 
-        if (isNonErrorException) {
-          // We want to ignore those kind of errors
-          return null;
-        }
-        if (beforeSend) {
-          return beforeSend(event, hint);
-        }
-        return event;
-      },
+          if (isNonErrorException) {
+            // We want to ignore those kind of errors
+            return null;
+          }
+          if (beforeSend) {
+            return beforeSend(event, hint);
+          }
+          return event;
+        },
 
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
-      ...options,
-    });
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+        ...options,
+      });
+    }
   },
 };
 
