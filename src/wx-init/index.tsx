2bd413b65699755dfdf5ed8bc1498e97d4bab1d1
mushan <mushan0x0@gmail.com>
2021-06-23 11:19:49 +0800

    feat: 添加wx-init

--- src/wx-init/index.tsx
+++ src/wx-init/index.tsx
@@ -1,26 +1,64 @@
 import axios from '../axios';
 import Sentry from '../sentry';
+import isWx from '../is-wx';
 const wx = require('weixin-js-sdk');
 
-export default () => {
-  const data = new FormData();
-  const url = encodeURIComponent(
-    window.location.href.split('#')[0] || window.location.href,
-  );
-  data.append('url', url);
-  return axios
-    .post('//ih.cqkqinfo.com/api/ehis/health/api/inquiry/getJsApiConfig', data)
-    .then(({ data: { data } }) => {
-      const config = {
-        debug: false,
-        beta: true,
-        ...data,
-        nonceStr: data.noncestr,
-      };
+interface ConfigData {
+  appId: string;
+  timestamp: number;
+  nonceStr: string;
+  signature: string;
+}
+
+export default ({
+  apiUrl,
+  configData,
+}: {
+  apiUrl?: string;
+  configData?: ConfigData;
+}) =>
+  new Promise((resolve, reject) => {
+    const data = new FormData();
+    const url = encodeURIComponent(
+      window.location.href.split('#')[0] || window.location.href,
+    );
+    data.append('url', url);
+    const init = async () => {
+      let config: any;
+      if (configData) {
+        config = configData;
+      } else {
+        await axios
+          .post(
+            apiUrl ||
+              '//ih.cqkqinfo.com/api/ehis/health/api/inquiry/getJsApiConfig',
+            data,
+          )
+          .then(({ data: { data } }) => {
+            config = {
+              debug: false,
+              beta: true,
+              ...data,
+              nonceStr: data.noncestr,
+            };
+          });
+      }
       wx.config(config);
       Sentry.setExtra('wxConfig', config);
       wx.error((res: any) => {
         Sentry.setExtra('wxError', res);
       });
-    });
-};
+      resolve({});
+    };
+    if (isWx) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      if (!window.WeixinJSBridge) {
+        document.addEventListener('WeixinJSBridgeReady', () => {
+          init();
+        });
+      } else {
+        init();
+      }
+    }
+  });
