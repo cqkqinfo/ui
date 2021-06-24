aecf58b124c8f24a529864fc768ec186674027f3
mushan <mushan0x0@gmail.com>
2021-06-25 00:20:52 +0800

    feat: page新增overflowFixed

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -3,7 +3,16 @@ import Space, { Props } from '../space';
 import styles from './index.module.less';
 import classNames from 'classnames';
 
-export default ({ className, ...props }: Props) => {
+export default ({
+  className,
+  overflowFixed,
+  ...props
+}: Props & {
+  /**
+   * 是否防止fixed跑到界面外
+   */
+  overflowFixed?: boolean;
+}) => {
   return (
     <Space
       flex={1}
@@ -12,6 +21,10 @@ export default ({ className, ...props }: Props) => {
       size={20}
       className={classNames(className, styles.page)}
       {...props}
+      style={{
+        transform: overflowFixed ? 'translateY(0px)' : undefined,
+        ...props.style,
+      }}
     />
   );
 };
