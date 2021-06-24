3d52bb96ac9b0fc3d3b37455368bb21f654283c4
mushan <mushan0x0@gmail.com>
2021-06-25 00:23:40 +0800

    feat: transfer change 支持 mode

--- src/transfer-change/index.tsx
+++ src/transfer-change/index.tsx
@@ -1,11 +1,51 @@
-import React from 'React';
+import React from 'react';
+import moment from 'moment';
 
 export default ({
   children,
   onChange = () => {},
   value,
+  mode,
 }: {
-  children: (onChange: (value: any) => void, value: any) => React.ReactElement;
-  onChange: (value: any) => void;
-  value: any;
-}) => children(onChange, value);
+  children:
+    | ((onChange: (value: any) => void, value: any) => any)
+    | React.ReactNode;
+  onChange?: (value: any) => void;
+  value?: any;
+  mode?: 'array' | 'date' | 'time' | 'JSON';
+}) => {
+  if (value !== undefined) {
+    if (mode === 'array') {
+      value = [value];
+    } else if (mode === 'date') {
+      value = value && moment(value, 'YYYY-MM-DD HH:mm:ss');
+    } else if (mode === 'time') {
+      value = value && moment(value, 'HH:mm');
+    } else if (mode === 'JSON') {
+      value = JSON.parse(value || '{}');
+    }
+  }
+  return mode && React.isValidElement(children)
+    ? React.cloneElement(children, {
+        ...children.props,
+        onChange: (value: any) => {
+          if (value === undefined) {
+            return onChange(value);
+          }
+          if (mode === 'array') {
+            value = value[0];
+          } else if (mode === 'date') {
+            value = value && moment(value).format('YYYY-MM-DD HH:mm:ss');
+          } else if (mode === 'time') {
+            value = value && moment(value).format('HH:mm');
+          } else if (mode === 'JSON') {
+            value = Object.keys(value).length
+              ? JSON.stringify(value)
+              : undefined;
+          }
+          onChange(value);
+        },
+        value,
+      })
+    : children instanceof Function && children(onChange, value);
+};
