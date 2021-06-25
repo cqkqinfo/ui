19daf0928006330179e0843413cee25afd7176ca
mushan <mushan0x0@gmail.com>
2021-06-25 11:33:55 +0800

    feat: mode支持city，新增dateFormat

--- src/transfer-change/index.tsx
+++ src/transfer-change/index.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import moment from 'moment';
+import getAddress from '../get-address';
+import getAddressId from '../get-address-id';
 
 export default ({
   children,
   onChange = () => {},
   value,
   mode,
+  dateFormat = 'YYYY-MM-DD HH:mm:ss',
 }: {
   children:
     | ((onChange: (value: any) => void, value: any) => any)
     | React.ReactNode;
   onChange?: (value: any) => void;
   value?: any;
-  mode?: 'array' | 'date' | 'time' | 'JSON';
+  mode?: 'array' | 'date' | 'time' | 'JSON' | 'city';
+  dateFormat?: string;
 }) => {
   if (value !== undefined) {
     if (mode === 'array') {
       value = [value];
     } else if (mode === 'date') {
-      value = value && moment(value, 'YYYY-MM-DD HH:mm:ss');
+      value = value && moment(value, dateFormat);
     } else if (mode === 'time') {
       value = value && moment(value, 'HH:mm');
     } else if (mode === 'JSON') {
       value = JSON.parse(value || '{}');
+    } else if (mode === 'city') {
+      value = getAddressId(value);
     }
   }
   return mode && React.isValidElement(children)
@@ -36,16 +42,18 @@ export default ({
           if (mode === 'array') {
             value = value[0];
           } else if (mode === 'date') {
-            value = value && moment(value).format('YYYY-MM-DD HH:mm:ss');
+            value = value && moment(value).format(dateFormat);
           } else if (mode === 'time') {
             value = value && moment(value).format('HH:mm');
           } else if (mode === 'JSON') {
             value = Object.keys(value).length
               ? JSON.stringify(value)
               : undefined;
+          } else if (mode === 'city') {
+            value = getAddress(value);
           }
           onChange(value);
-          children.props?.onChange(...arg);
+          children.props.onChange?.(...arg);
         },
         value,
       })
