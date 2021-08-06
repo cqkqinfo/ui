c0444a051b3ca73f438e5fe84bc00cecea2cb616
mushan <mushan0x0@gmail.com>
2021-08-06 11:20:20 +0800

    feat: 添加scroll事件

--- src/scroll-view/index.tsx
+++ src/scroll-view/index.tsx
@@ -2,9 +2,23 @@ import { ScrollViewProps } from 'remax/wechat';
 import React, { PropsWithChildren, useEffect, forwardRef } from 'react';
 import classNames from 'classnames';
 import styles from './index.module.less';
+import Visible from '../visible';
+import Space from '@/space';
 
 export default forwardRef<HTMLDivElement, PropsWithChildren<ScrollViewProps>>(
-  ({ scrollIntoView, onTap, children, className, scrollY, ...props }, ref) => {
+  (
+    {
+      scrollIntoView,
+      onTap,
+      onScrollToLower,
+      onScrollToUpper,
+      children,
+      className,
+      scrollY,
+      ...props
+    },
+    ref,
+  ) => {
     useEffect(() => {
       const target =
         scrollIntoView && document.querySelector(`#${scrollIntoView}`);
@@ -19,7 +33,13 @@ export default forwardRef<HTMLDivElement, PropsWithChildren<ScrollViewProps>>(
         {...(props as any)}
         onClick={onTap}
         ref={ref}
-      />
+      >
+        <Space flex={1} justify={'space-between'} vertical={scrollY}>
+          <Visible onVisible={() => onScrollToUpper?.({} as any)} />
+          {children}
+          <Visible onVisible={() => onScrollToLower?.({} as any)} />
+        </Space>
+      </div>
     );
   },
 );
