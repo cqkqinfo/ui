76efbd87f0648fb0d701a8ba6b8d41fc92b934df
xjxsanto <xjxsanto@parsec.com.cn>
2021-08-15 14:47:27 +0800

    fix: fix

---
+++ src/use-view-size/index.ali.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
+import { createSelectorQuery } from 'remax/ali';
+
+export const getWH = (id: string) =>
+  new Promise(resolve => {
+    const query = createSelectorQuery();
+    query
+      .select(`#${id}`)
+      .boundingClientRect()
+      .exec(ret => {
+        console.log(ret);
+        return ret;
+      });
+  });
+
+export default (id: string) => {
+  const [wh, setWH] = useState<{
+    width?: number;
+    height?: number;
+  }>({});
+  const countRef = useRef(0);
+  useEffect(() => {
+    const query = createSelectorQuery();
+    const count = countRef.current + 1;
+    countRef.current = count;
+    query
+      .select(`#${id}`)
+      .boundingClientRect()
+      .exec((data: any) => {
+        console.log(data);
+        const ret = data[0] || {};
+        if (ret && (ret?.width !== wh.width || ret?.height !== wh.height)) {
+          if (count === countRef.current) {
+            setWH(ret);
+          }
+        }
+      });
+  });
+  return wh;
+};
