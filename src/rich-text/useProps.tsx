aedc647e661bfa0629e235fb3ca6d1e1eadcb914
mushan <mushan0x0@gmail.com>
2021-08-17 13:41:37 +0800

    fix: 图片无法加载

--- src/rich-text/useProps.tsx
+++ src/rich-text/useProps.tsx
@@ -13,7 +13,8 @@ export default ({
     className: classNames(className, styles.text),
     space,
     nodes: (nodes + '')
-      ?.replace(/\.(png)|(jpg)/gi, `.$1?x-oss-process=image/resize,w_${1080}`)
+      ?.replace(/\.png/gi, `.png?x-oss-process=image/resize,w_${1080}`)
+      ?.replace(/\.jpg/gi, `.jpg?x-oss-process=image/resize,w_${1080}`)
       ?.replace(/<img/g, '<img style="width: 100%"')
       ?.replace(/<p/g, '<p style="margin-top: 10px;margin-bottom: 10px;"'),
   };
