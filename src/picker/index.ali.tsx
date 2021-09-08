73c62304e5d0b3bceb59ad187db6ddfade52f011
jaluik <cqzhengmouren@gmail.com>
2021-09-08 10:33:15 +0800

    fix: 修复ali小程序picker选择问题

--- src/picker/index.ali.tsx
+++ src/picker/index.ali.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { multiLevelSelect, View } from 'remax/ali';
+import { multiLevelSelect, View, datePicker, optionsSelect } from 'remax/ali';
 import { Props, useProps } from './common';
 
 const getList = (data: Props['data']): any =>
@@ -11,33 +11,68 @@ const getList = (data: Props['data']): any =>
 export default (props: Props) => {
   const {
     title,
+    cols = 1,
     data,
     onChange,
+    mode = 'selector',
     children,
+    value,
+    start,
+    end,
     childrenCls,
     ...newProps
   } = useProps(props);
+
   return (
     <View
       {...newProps}
       className={childrenCls}
-      onTap={() =>
-        multiLevelSelect({
-          title: title as any,
-          name: '',
-          list: getList(data),
-          fail: () => onChange(undefined),
-          success: ({ result }: any) => {
-            const items: any = [];
-            result.forEach(({ name }: any, i: any) => {
-              items[i] = (items[i - 1]?.children || data)
-                ?.flat()
-                .find(({ label }: any) => label === name);
-            });
-            onChange(items.map(({ value }: any) => value));
-          },
-        })
-      }
+      onTap={() => {
+        if (mode === 'multiSelector' || cols !== 1) {
+          multiLevelSelect({
+            title: title as any,
+            name: '',
+            list: getList(data),
+            fail: () => onChange(undefined),
+            success: ({ result }: any) => {
+              const items: any = [];
+              result.forEach(({ name }: any, i: any) => {
+                items[i] = (items[i - 1]?.children || data)
+                  ?.flat()
+                  .find(({ label }: any) => label === name);
+              });
+              onChange(items.map(({ value }: any) => value));
+            },
+          });
+        }
+        if (mode === 'selector' && cols === 1) {
+          const list = getList(data);
+          optionsSelect({
+            title: title as any,
+            selectedOneIndex: list.findIndex(
+              (item: { label: string; value: string }) => item.value === value,
+            ),
+            optionsOne: list.map(
+              (item: { label: string; value: string }) => item.label,
+            ),
+            success: ({ selectedOneIndex }) => {
+              onChange(list[selectedOneIndex]?.value);
+            },
+          });
+        }
+        if (['time', 'date'].includes(mode)) {
+          const format = mode === 'time' ? 'HH:mm' : 'yyyy-MM-dd';
+          datePicker({
+            format,
+            currentDate: value as string,
+            startDate: start,
+            endDate: end,
+            success: res => {
+              onChange?.(res.date);
+            },
+          });
+        }
+      }}
     >
       {children}
     </View>
