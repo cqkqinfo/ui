eb8350db631921f1af0016a5911510f4485389fb
jaluik <cqzhengmouren@gmail.com>
2021-09-15 15:44:00 +0800

    feat: 更新icon组件

--- src/icon/web/KqAdd.tsx
+++ src/icon/web/KqAdd.tsx
@@ -13,22 +13,11 @@ const DEFAULT_STYLE: CSSProperties = {
   display: 'block',
 };
 
-const KqAdd: FunctionComponent<Props> = ({
-  size,
-  color,
-  style: _style,
-  ...rest
-}) => {
+const KqAdd: FunctionComponent<Props> = ({ size, color, style: _style, ...rest }) => {
   const style = _style ? { ...DEFAULT_STYLE, ..._style } : DEFAULT_STYLE;
 
   return (
-    <svg
-      viewBox="0 0 1024 1024"
-      width={size + 'px'}
-      height={size + 'px'}
-      style={style}
-      {...rest}
-    >
+    <svg viewBox="0 0 1024 1024" width={size + 'px'} height={size + 'px'} style={style} {...rest}>
       <path
         d="M583.55791267 440.44208733L583.55791267 11.42977713 440.44208733 11.42977713 440.44208733 440.44208733 11.42977713 440.44208733 11.42977713 583.55791267 440.44208733 583.55791267 440.44208733 1012.57022287 583.55791267 1012.57022287 583.55791267 583.55791267 1012.57022287 583.55791267 1012.57022287 440.44208733Z"
         fill={getIconColor(color, 0, '#333333')}
