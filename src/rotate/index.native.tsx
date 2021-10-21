af4dfc7bb2944e4278894e1ad1b60b6904a87087
mushan <mushan0x0@gmail.com>
2021-10-21 10:44:26 +0800

    refactor: 重构rn的rotate

--- src/rotate/index.native.tsx
+++ src/rotate/index.native.tsx
@@ -2,27 +2,52 @@ import classNames from 'classnames';
 import React from 'react';
 import styles from './index.module.less';
 import { Props } from './index';
+import { useState } from 'react';
+import { Animated, Easing } from 'react-native';
+import { useEffect } from 'react';
 import { View } from 'remax/one';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import RotateView from 'react-native-rotate-view';
 
 export default ({
   className,
-  angle = 360,
+  angle,
   run = true,
   children,
+  style,
   ...props
 }: Props) => {
+  const [rotateValue] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    const toValue = run ? 1 : 0;
+    Animated.loop(
+      Animated.timing(rotateValue, {
+        toValue: toValue,
+        duration: angle === undefined ? 1000 : 300,
+        easing: angle === undefined ? Easing.linear : undefined,
+        useNativeDriver: true,
+      }),
+      { iterations: angle === undefined ? -1 : 1 },
+    ).start();
+  }, [angle, rotateValue, run]);
+
+  const spin = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', `${angle || 360}deg`],
+  });
   return (
-    <RotateView
-      rotate={run}
-      degree={`${angle}deg`}
-      duration={300}
+    <Animated.View
+      style={[
+        {
+          transform: [{ rotate: spin }],
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        style,
+      ]}
       className={classNames(angle === undefined && styles.rotate, className)}
-      {...props}
+      {...(props as any)}
     >
       <View>{children}</View>
-    </RotateView>
+    </Animated.View>
   );
 };
