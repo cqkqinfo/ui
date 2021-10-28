3efdc16a4595407f7cd379bfc46487e5f94828bf
mushan <mushan0x0@gmail.com>
2021-10-28 10:35:08 +0800

    fix: 获取城市数据没有返回市、区
    (cherry picked from commit 725a842cf79397e198b1bd56e17dda9c6030bffb)

--- src/get-address-options/index.tsx
+++ src/get-address-options/index.tsx
@@ -12,42 +12,46 @@ export interface CascaderOptionType {
 
 let areas: any;
 let transform = false;
+let pending: any = Promise.resolve();
 let options: any = [];
 
 export default async () => {
   if (!areas) {
     areas = [];
-    ({ data: areas } = await axios.get(
+    pending = axios.get(
       'https://kq-static.oss-cn-beijing.aliyuncs.com/ui/areas.json',
-    ));
-    if (!transform) {
-      transform = true;
-      areas.forEach((area: any) => {
-        const matchCity: CascaderOptionType = cities.filter(
-          city => city.code === area.cityCode,
-        )[0];
-        if (matchCity) {
-          matchCity.children = matchCity.children || [];
-          matchCity.children.push({
-            label: area.name,
-            value: area.code,
-          });
-        }
-      });
-      cities.forEach((city: CascaderOptionType) => {
-        const matchProvince: CascaderOptionType = provinces.filter(
-          province => province.code === city.provinceCode,
-        )[0];
-        if (matchProvince) {
-          matchProvince.children = matchProvince.children || [];
-          matchProvince.children.push({
-            label: city.name,
-            value: city.code,
-            children: city.children,
-          });
-        }
-      });
-    }
+    );
+    ({ data: areas } = await pending);
+  } else {
+    await pending;
+  }
+  if (!transform) {
+    transform = true;
+    areas.forEach((area: any) => {
+      const matchCity: CascaderOptionType = cities.filter(
+        city => city.code === area.cityCode,
+      )[0];
+      if (matchCity) {
+        matchCity.children = matchCity.children || [];
+        matchCity.children.push({
+          label: area.name,
+          value: area.code,
+        });
+      }
+    });
+    cities.forEach((city: CascaderOptionType) => {
+      const matchProvince: CascaderOptionType = provinces.filter(
+        province => province.code === city.provinceCode,
+      )[0];
+      if (matchProvince) {
+        matchProvince.children = matchProvince.children || [];
+        matchProvince.children.push({
+          label: city.name,
+          value: city.code,
+          children: city.children,
+        });
+      }
+    });
   }
   if (!options.length) {
     options = provinces.map((province: CascaderOptionType) => ({
@@ -56,6 +60,5 @@ export default async () => {
       children: province.children,
     }));
   }
-
   return JSON.parse(JSON.stringify(options)) as PickerData[];
 };
