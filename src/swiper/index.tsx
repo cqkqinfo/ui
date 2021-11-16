a4a6b22d8591737e709f8216d4769c4e1f0a370b
mushan <mushan0x0@gmail.com>
2021-11-16 14:17:03 +0800

    feat: swiper 可以自己控制
    (cherry picked from commit 12314520653e7032d27295c6f148b7b6a06dbf5c)

--- src/swiper/index.tsx
+++ src/swiper/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SwiperProps, SwiperItemProps } from 'remax/wechat';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import 'swiper/swiper.less';
@@ -7,6 +7,7 @@ import 'swiper/modules/pagination/pagination.less';
 import SwiperCore, { Pagination, Autoplay } from 'swiper';
 import styles from './index.module.less';
 import classNames from 'classnames';
+import { Swiper as SwiperClass } from 'swiper/types';
 
 // install Swiper modules
 SwiperCore.use([Pagination, Autoplay]);
@@ -23,15 +24,26 @@ export default ({
   interval = 3000,
   indicatorDots,
   onChange,
+  displayMultipleItems = 1,
+  current = 0,
   ...props
 }: Props) => {
+  useEffect(() => {}, []);
+  const [swiperRef, setSwiperRef] = useState<SwiperClass>();
+  useEffect(() => {
+    if (!swiperRef) return;
+    swiperRef.slideTo(current);
+  }, [current, swiperRef]);
   return (
     <Swiper
+      onSwiper={setSwiperRef}
       className={className}
       style={style}
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       onSlideChange={e => onChange?.({ detail: { current: e.activeIndex } })}
+      centeredSlides
+      slidesPerView={displayMultipleItems}
       pagination={
         indicatorDots
           ? {
