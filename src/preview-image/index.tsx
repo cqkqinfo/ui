5b5c762bf58f0705af20bedff9d09fbab0756407
mushan <mushan0x0@gmail.com>
2021-12-02 00:49:54 +0800

    feat: 预览图片兼容rn
    (cherry picked from commit f2adcf33c7438c8321478537f036b030e0e62d10)

--- src/preview-image/index.tsx
+++ src/preview-image/index.tsx
@@ -1,29 +1,4 @@
-import { Options } from './index.wechat';
-import ReactDOM from 'react-dom';
-import { PhotoSlider } from 'react-photo-view';
-import 'react-photo-view/dist/index.css';
-import React from 'react';
-import './index.less';
-import { useEffectState } from 'parsec-hooks';
+import { previewImage } from 'remax/wechat';
 
-const Component = ({ urls, current, dom }: Options & { dom: HTMLElement }) => {
-  const [index, setIndex] = useEffectState(
-    current ? urls.findIndex(i => i === current) : 0,
-  );
-  return (
-    <PhotoSlider
-      images={urls.map(item => ({ src: item }))}
-      visible
-      onIndexChange={setIndex}
-      index={index}
-      onClose={() => {
-        ReactDOM.unmountComponentAtNode(dom);
-      }}
-    />
-  );
-};
-
-export default (options: Options) => {
-  const dom = document.createElement('div');
-  ReactDOM.render(<Component {...options} dom={dom} />, dom);
-};
+export type Options = WechatMiniprogram.PreviewImageOption;
+export default (options: Options) => previewImage(options);
--- src/preview-image/index.wechat.tsx
+++
@@ -1,4 +0,0 @@
-import { previewImage } from 'remax/wechat';
-
-export type Options = WechatMiniprogram.PreviewImageOption;
-export default (options: Options) => previewImage(options);
