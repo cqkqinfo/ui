f6d189ea31c2557ec764a3f67ff6be7da599e3c7
mushan <mushan0x0@gmail.com>
2021-12-09 11:31:10 +0800

    chore: visible web改用ahooks实现

--- src/visible/index.tsx
+++ src/visible/index.tsx
@@ -1,36 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import Visible from './native';
-import React from 'react';
-import { View } from 'remax/wechat';
+import React, { useEffect, useRef } from 'react';
+import { useInViewport } from 'ahooks';
+import { View } from 'remax/one';
 
-export interface Props {
+interface Props {
   onVisible?: () => void;
   onHidden?: () => void;
   className?: string;
   children?: React.ReactNode;
-  /**
-   * 优化性能
-   */
-  perf?: boolean;
-  height?: number;
 }
 
-export default ({
-  children,
-  className,
-  onHidden,
-  onVisible,
-  height,
-  perf,
-}: Props) => (
-  <Visible
-    perf={perf}
-    class-name={className}
-    bindvisible={onVisible}
-    height={height}
-    bindhidden={onHidden}
-  >
-    {children || <View style={{ height: '1px', width: '1px' }} />}
-  </Visible>
-);
+export default ({ children, className, onHidden, onVisible }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const visible = useInViewport(ref);
+  useEffect(() => {
+    if (visible) {
+      onVisible?.();
+    } else {
+      onHidden?.();
+    }
+  }, [visible, onVisible, onHidden]);
+  return (
+    <div ref={ref} className={className}>
+      {children || <View style={{ height: '1px', width: '1px' }} />}
+    </div>
+  );
+};
---
+++ src/visible/index.wechat.tsx
@@ -0,0 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Visible from './native';
+import React from 'react';
+import { View } from 'remax/wechat';
+
+export interface Props {
+  onVisible?: () => void;
+  onHidden?: () => void;
+  className?: string;
+  children?: React.ReactNode;
+  /**
+   * 优化性能
+   */
+  perf?: boolean;
+  height?: number;
+}
+
+export default ({
+  children,
+  className,
+  onHidden,
+  onVisible,
+  height,
+  perf,
+}: Props) => (
+  <Visible
+    perf={perf}
+    class-name={className}
+    bindvisible={onVisible}
+    height={height}
+    bindhidden={onHidden}
+  >
+    {children || <View style={{ height: '1px', width: '1px' }} />}
+  </Visible>
+);
