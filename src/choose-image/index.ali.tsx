14a6bcd2bb9cd20ae1ed927bd78854842cdb8eb6
lwc <liuwenchao@cqkqinfo.com>
2021-12-19 22:18:47 +0800

    feat: 新增choose-image、open-data、naviagte-to-mini-program、scroll-view、upload-file、user-authorization支付环小程序支持

---
+++ src/choose-image/index.ali.tsx
@@ -0,0 +1,4 @@
+import { chooseImage } from 'remax/ali';
+
+export type Options = my.IChooseImageOptions;
+export default (options?: Options) => chooseImage(options);
--- src/open-type-button/index.ali.tsx
+++ src/open-type-button/index.ali.tsx
@@ -2,6 +2,17 @@ import { View } from 'remax/one';
 import React from 'react';
 import { Props } from './index';
 
-export default ({ children, type }: Props) => {
-  return <View>{children}</View>;
+export default ({ children, type, onGetUserInfo, ...props }: Props) => {
+  return (
+    <View
+      {...props}
+      onTap={() => {
+        if (type === 'getUserInfo') {
+          onGetUserInfo?.({});
+        }
+      }}
+    >
+      {children}
+    </View>
+  );
 };
