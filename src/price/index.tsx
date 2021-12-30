8cda4b176f5fe50e81bfa3e86e4a17bb2f701a09
mushan <mushan0x0@gmail.com>
2021-12-30 17:16:48 +0800

    feat: 添加金额组件

---
+++ src/price/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Space } from '@kqinfo/ui';
+import { Props as SpaceProps } from '../space';
+import styles from './index.module.less';
+import classNames from 'classnames';
+import getPrice from './getPrice';
+
+export interface Props extends SpaceProps {
+  /**
+   * 金额，单位：分
+   */
+  price?: number;
+  /**
+   * 小数位长度
+   * @default 2
+   */
+  len?: number;
+  /**
+   * 粗体
+   */
+  bold?: boolean;
+  /**
+   * 前缀大字体
+   */
+  bigPrefix?: boolean;
+  /**
+   * 小数位大字体
+   */
+  bigDecimal?: boolean;
+  /**
+   * 字体放大比例
+   * @default 1.53
+   */
+  bigScale?: number;
+  /**
+   * 大字体类名
+   */
+  bigCls?: string;
+  /**
+   * 整数类名
+   */
+  integerCls?: string;
+  /**
+   * 小数类名
+   */
+  decimalCls?: string;
+}
+
+export default ({
+  price,
+  className,
+  len,
+  bigPrefix,
+  bigDecimal,
+  integerCls,
+  decimalCls,
+  bigScale = 1.53,
+  bigCls,
+  ...props
+}: Props) => {
+  const str = getPrice(price, len);
+  const [integer, decimal] = str.split('.');
+  const bigStyle = { fontSize: `${bigScale}em` };
+  return (
+    <Space
+      className={classNames(styles.price, className)}
+      alignItems={'baseline'}
+      {...props}
+    >
+      <Space
+        className={classNames(bigPrefix && bigCls)}
+        style={bigPrefix ? bigStyle : {}}
+      >
+        ¥
+      </Space>
+      <Space className={classNames(bigCls, integerCls)} style={bigStyle}>
+        {integer}
+      </Space>
+
+      <Space
+        className={classNames(bigDecimal && bigCls, decimalCls)}
+        style={bigDecimal ? bigStyle : {}}
+      >
+        .{decimal}
+      </Space>
+    </Space>
+  );
+};
