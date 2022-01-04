84dacf0ec4d2b2d822b1cd5dc0759f9dfc5685a7
mushan <mushan0x0@gmail.com>
2022-01-04 14:51:54 +0800

    feat: 分割线支持设置 lineCls

--- src/divider/index.tsx
+++ src/divider/index.tsx
@@ -2,23 +2,31 @@ import React from 'react';
 import Space, { Props as SpaceProps } from '../space';
 import rpxToPx from '../rpx-to-px';
 import { useConfig } from '../config-provider';
+import styles from './index.module.less';
+import classnames from 'classnames';
 
 export default ({
   children,
   color,
   style,
+  lineCls,
   ...props
 }: SpaceProps & {
   /**
    * 颜色
    */
   color?: string;
+  /**
+   * 分割线类名
+   */
+  lineCls?: string;
 }) => {
   const { brandPrimary } = useConfig();
   const showColor = color || brandPrimary;
   const line = (
     <Space
-      style={{ height: 1, width: rpxToPx(58), backgroundColor: showColor }}
+      style={{ height: 1, backgroundColor: showColor }}
+      className={classnames(styles.line, lineCls)}
     />
   );
   return (
