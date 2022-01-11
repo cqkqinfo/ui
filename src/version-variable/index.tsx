dcf77fb4504530e13851cca485acd0a22f2db68d
mushan <mushan0x0@gmail.com>
2022-01-11 17:22:30 +0800

    fix: versionVariable类型

--- src/version-variable/index.tsx
+++ src/version-variable/index.tsx
@@ -4,13 +4,13 @@ export default <D extends unknown>(obj: {
   /**
    * 开发版变量
    */
-  develop: Partial<D>;
+  develop: D;
   /**
    * 体验版变量
    */
-  trial?: Partial<D>;
+  trial?: D;
   /**
    * 正式版变量
    */
-  release?: Partial<D>;
-}) => obj[envVersion] || obj['develop'];
+  release?: D;
+}) => (obj[envVersion] || obj['develop']) as D;
