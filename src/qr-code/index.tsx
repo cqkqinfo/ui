12198cee13b3c562695abfc6c6cd8ee37cfbc872
mushan <mushan0x0@gmail.com>
2022-01-18 17:03:50 +0800

    feat: 二维码支持手动显示
    (cherry picked from commit 6d1a5256bd19516f5872f95e7a5cce665d753120)

--- src/qr-code/index.tsx
+++ src/qr-code/index.tsx
@@ -1,27 +1,30 @@
 import QrCode from './one';
 import QrCodeProps from './one/common';
-import React, { useRef } from 'react';
+import React, { useRef, forwardRef, useImperativeHandle } from 'react';
 import Modal from './modal';
 import { SheetInstance } from '../sheet';
 
 export { default as QrCodeProps } from './one/common';
 
-export default ({ showModal, modalTitle, onTap, ...props }: QrCodeProps) => {
-  const ref = useRef<SheetInstance>(null);
-  return (
-    <>
-      <QrCode
-        {...props}
-        onTap={e => {
-          if (showModal) {
-            ref.current?.setVisible(true);
-          }
-          onTap?.(e);
-        }}
-      />
-      {showModal && (
-        <Modal ref={ref} content={props.content} title={modalTitle} />
-      )}
-    </>
-  );
-};
+export default forwardRef(
+  ({ showModal, modalTitle, onTap, ...props }: QrCodeProps, ref) => {
+    const innerRef = useRef<SheetInstance>(null);
+    useImperativeHandle(ref, () => innerRef.current, [innerRef]);
+    return (
+      <>
+        <QrCode
+          {...props}
+          onTap={e => {
+            if (showModal) {
+              innerRef.current?.setVisible(true);
+            }
+            onTap?.(e);
+          }}
+        />
+        {showModal && (
+          <Modal ref={innerRef} content={props.content} title={modalTitle} />
+        )}
+      </>
+    );
+  },
+);
