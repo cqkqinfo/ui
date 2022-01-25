af973d3001d20b0bcf92368cbee97b0f23f7eeb4
mushan <mushan0x0@gmail.com>
2022-01-25 17:30:50 +0800

    feat: scroll支持隐藏滚动条

--- src/scroll-view/index.tsx
+++ src/scroll-view/index.tsx
@@ -1,3 +1,13 @@
 import { ScrollView } from 'remax/wechat';
+import { ScrollViewProps } from '@remax/wechat/esm/hostComponents/ScrollView';
+import React from 'react';
+import classNames from 'classnames';
+import styles from './index.module.less';
 
-export default ScrollView;
+export default ({ className, showScrollbar, ...props }: ScrollViewProps) => (
+  <ScrollView
+    {...props}
+    className={classNames(className, showScrollbar === false && styles.hideBar)}
+    showScrollbar={showScrollbar}
+  />
+);
