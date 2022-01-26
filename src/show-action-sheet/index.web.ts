66eb25a17fabf0b1ee37b7129f78e9d948d6f054
mushan <mushan0x0@gmail.com>
2022-01-26 10:30:44 +0800

    fix: showActionsSheet不能关闭
    (cherry picked from commit 95e6172df9f0b8b114138e2200607e0f7e4ca995)

--- src/show-action-sheet/index.web.ts
+++ src/show-action-sheet/index.web.ts
@@ -1,14 +1,13 @@
 import { ActionSheet } from 'antd-mobile';
 
 export default ({ itemList }: WechatMiniprogram.ShowActionSheetOption) =>
-  new Promise((resolve, reject) =>
-    ActionSheet.show({
+  new Promise((resolve, reject) => {
+    const sheet = ActionSheet.show({
       actions: [...itemList].map(text => ({ text, key: text })),
       cancelText: '取消',
       onAction: (_, index) => {
-        if (index !== itemList.length) {
-          resolve({ tapIndex: index });
-        }
+        resolve({ tapIndex: index });
+        sheet.close();
       },
-    }),
-  );
+    });
+  });
