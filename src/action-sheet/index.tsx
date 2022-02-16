8f39ae380ce1d28dade0097c7cf49cc183e7432f
mushan <mushan0x0@gmail.com>
2022-02-16 11:22:07 +0800

    feat: action-sheet支持title

--- src/action-sheet/index.tsx
+++ src/action-sheet/index.tsx
@@ -4,25 +4,46 @@ import { SheetWrap, SheetWrapInstance, SheetWrapData } from '../sheet';
 import { View } from 'remax/one';
 import styles from './index.module.less';
 import Space from '../space';
+import classNames from 'classnames';
 
 export interface ShowOptions {
+  /**
+   * 操作项
+   */
   items: { label: React.ReactNode; value: string | number }[];
+  /**
+   * 标题
+   */
+  title?: React.ReactNode;
+  /**
+   * 标题类名
+   */
+  titleCls?: string;
+  /**
+   * 子项类名
+   */
+  itemCls?: string;
 }
 
 const data: SheetWrapData = {};
 
 const ActionSheet = () => {
-  const [options, setOptions] = useState<{ items: ShowOptions['items'] }>({
+  const [{ title, titleCls, itemCls, items }, setOptions] = useState<
+    ShowOptions
+  >({
     items: [],
   });
   const ref = useRef<SheetWrapInstance>(null);
   return (
     <SheetWrap ref={ref} setOptions={setOptions} data={data}>
       <Space vertical style={{ width: '100vw' }}>
-        {options.items.map(({ label, value }, index) => (
+        {title && (
+          <View className={classNames(titleCls, styles.title)}>{title}</View>
+        )}
+        {items.map(({ label, value }, index) => (
           <React.Fragment key={index}>
             <View
-              className={styles.item}
+              className={classNames(itemCls, styles.item)}
               onTap={() => {
                 ref.current?.sheetRef?.setVisible(false);
                 ref.current?.promiseRef.resolve({ label, value });
@@ -30,9 +51,7 @@ const ActionSheet = () => {
             >
               {label}
             </View>
-            {index !== options.items.length - 1 && (
-              <View className={styles.line} />
-            )}
+            {index !== items.length - 1 && <View className={styles.line} />}
           </React.Fragment>
         ))}
         <View
