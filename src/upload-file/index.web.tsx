c43c71fc11665e3cd2ee13e83ca858c59704c5c1
mushan <mushan0x0@gmail.com>
2022-03-04 13:47:43 +0800

    fix: web端uploadFile报错

--- src/upload-file/index.web.tsx
+++ src/upload-file/index.web.tsx
@@ -2,6 +2,7 @@ import axios from '../axios';
 
 export default ({
   formData: data,
+  filePath,
   name,
   url,
 }: WechatMiniprogram.UploadFileOption) => {
@@ -9,7 +10,7 @@ export default ({
   Object.keys(data as any).forEach(key => {
     formData.append(key, (data as any)[key]);
   });
-  formData.append(name, (data as any).file, (data as any).file.name);
+  formData.append(name, filePath, (filePath as any).name);
   return axios
     .post(url, formData, {
       headers: { Accept: '*/*' },
