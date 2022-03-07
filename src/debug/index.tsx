7eb9ae46facae03119a10464c59d3e3920193cbc
mushan <mushan0x0@gmail.com>
2022-03-07 16:21:21 +0800

    feat: 升级debug工具

--- src/debug/index.tsx
+++ src/debug/index.tsx
@@ -2,17 +2,17 @@
 // @ts-ignore
 import ImportCDNJS from 'import-cdn-js';
 import { envVersion } from '../get-version';
-import './index.less';
 
-export default (
-  showDebug = ['develop', 'trial'].includes(envVersion) ||
+export default (showDebug = ['develop', 'trial'].includes(envVersion)) => {
+  if (
+    showDebug ||
     window.location.hostname === '122.9.36.145' ||
-    window.location.href.includes('isDebug=true'),
-) => {
-  if (showDebug) {
-    ImportCDNJS('//cdn.bootcss.com/eruda/1.4.4/eruda.min.js', 'eruda').then(
+    window.location.href.includes('isDebug=true')
+  ) {
+    ImportCDNJS('//cdn.bootcss.com/eruda/2.4.1/eruda.min.js', 'eruda').then(
       (eruda: any) => {
         eruda.init();
+        eruda.util.evalCss('#eruda * { user-select: text !important; }');
       },
     );
   }
