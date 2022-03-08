8685fed456f02fa4a03609540179fbbb1c005fe8
mushan <mushan0x0@gmail.com>
2022-03-08 14:22:06 +0800

    fix: 移除 FastClick 修复上传需要点击多次才粗发

--- src/_init/index.web.tsx
+++ src/_init/index.web.tsx
@@ -1,6 +1,2 @@
 import 'antd-mobile/2x/es/global';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import FastClick from 'fastclick';
-
-FastClick.attach(document.body);
+import './index.less';
