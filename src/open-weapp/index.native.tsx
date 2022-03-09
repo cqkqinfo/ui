32fedfa9408734830a4b0e356b7f2b0d98a83e64
mushan <mushan0x0@gmail.com>
2022-03-09 14:03:52 +0800

    feat: openWeapp兼容rn

--- src/open-weapp/index.native.tsx
+++ src/open-weapp/index.native.tsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import { View, navigateTo } from 'remax/one';
+import { navigateTo } from 'remax/one';
 import { Props } from './index';
 
 export default ({
   // appId = getAccountInfoSync().miniProgram.appId,
   path,
   onLaunch,
+  children,
+  style,
   ...props
 }: Props) => {
-  return (
-    <View
-      {...props}
-      onTap={e => {
-        props.onTap?.(e);
-        if (path) {
-          // if (appId === getAccountInfoSync().miniProgram.appId) {
-          navigateTo({ url: path }).then(onLaunch);
-          // } else {
-          //   navigateToMiniProgram({
-          //     appId,
-          //     path,
-          //   }).then(onLaunch);
-          // }
-        }
-      }}
-    />
-  );
+  return children
+    ? React.cloneElement(children, {
+        ...children.props,
+        ...props,
+        style: {
+          ...children.props?.style,
+          ...style,
+        },
+        onTap: (e: any) => {
+          children.props.onTap?.(e);
+          if (path) {
+            // if (appId === getAccountInfoSync().miniProgram.appId) {
+            navigateTo({ url: path }).then(onLaunch);
+            // } else {
+            //   navigateToMiniProgram({
+            //     appId,
+            //     path,
+            //   }).then(onLaunch);
+            // }
+          }
+        },
+      })
+    : children;
 };
