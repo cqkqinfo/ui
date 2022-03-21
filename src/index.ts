32d7afe4f59bb0903e942a5b9681bbc4f00a2224
mushan <mushan0x0@gmail.com>
2022-03-21 23:21:55 +0800

    refactor<addressOptions>: 使用本地json，移除china-division、addressOptions

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ console.error(
  */
 export { default as Picker } from './picker';
 /**
- * 城市数据
- */
-export { default as addressOptions } from './address-options';
-/**
  * 根据城市数据的value获取城市名称
  */
 export { default as getAddress } from './get-address';
