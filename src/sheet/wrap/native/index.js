0896978bc8add15fbd5c4fcb4f2ad13f225e9d42
mushan <mushan0x0@gmail.com>
2022-03-24 16:32:49 +0800

    feat: sheet禁止背景滚动
    (cherry picked from commit 38d217565097cd4a5dc0adfb5f2df7c6b2fd1ede)

---
+++ src/sheet/wrap/native/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+Component({
+  options: {
+    multipleSlots: true, // 在组件定义时的选项中启用多slot支持
+  },
+  methods: {
+    touchMove() {
+      return false;
+    },
+  },
+});
