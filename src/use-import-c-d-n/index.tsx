5f934a88ce4d822cbf4007d8a88f9a08a3171c38
mushan <mushan0x0@gmail.com>
2022-03-31 17:33:46 +0800

    feat: importCDN支持运行es6代码
    (cherry picked from commit 77388fcda62e04c8ac983c70cf0100e1d0ebbb4e)

--- src/use-import-c-d-n/index.tsx
+++ src/use-import-c-d-n/index.tsx
@@ -2,16 +2,29 @@ import { useEffect, useState } from 'react';
 import importCDN from '../import-c-d-n';
 
 export default function<D = any>(
+  /**
+   * cdn链接
+   */
   link: string,
+  /**
+   * 引入的js文件名
+   */
   name: string,
-  callBack?: (js: D) => void,
+  /**
+   * 是否是es6模块
+   */
+  es6?: boolean,
 ) {
   const [code, setCode] = useState<D>();
   useEffect(() => {
-    importCDN(link, name, code => {
-      callBack?.(code);
-      setCode(code);
-    });
-  }, [link, name, callBack]);
+    importCDN(
+      link,
+      name,
+      code => {
+        setCode(code);
+      },
+      es6,
+    );
+  }, [es6, link, name]);
   return code;
 }
