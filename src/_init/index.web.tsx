5bacfdb0c8cf0f0b349acc0099a04e68f2aa5b38
mushan <mushan0x0@gmail.com>
2022-04-02 13:28:21 +0800

    feat: 兼容object.entries
    (cherry picked from commit 61219db92472375f54853c375a92fe95b435dc37)

--- src/_init/index.web.tsx
+++ src/_init/index.web.tsx
@@ -1,7 +1,3 @@
 import 'antd-mobile/2x/es/global';
 import './index.less';
-const entries = require('object.entries');
-
-if (!Object.entries) {
-  entries.shim();
-}
+require('es7-object-polyfill');
