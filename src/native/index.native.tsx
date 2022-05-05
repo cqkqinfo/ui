f72a7a8618f72b36998ffaa7605795ff4ff8ccac
mushan <mushan0x0@gmail.com>
2022-05-05 11:13:50 +0800

    pref: 优化rn native组件性能

--- src/native/index.native.tsx
+++ src/native/index.native.tsx
@@ -1,23 +1,60 @@
-import { forwardRef, useImperativeHandle, useState } from 'react';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 import React from 'react';
 import { NativeInstance, Props } from './index';
 import { View } from 'remax/one';
+import { useForceUpdate } from 'parsec-hooks';
 
 export default React.memo(
   forwardRef<NativeInstance, Props>(
     ({ initData = {}, children, onTap }, ref) => {
-      const [{ style, content, className, visible = true }, setData] = useState(
-        initData,
-      );
-      const [returns, setReturns] = useState<NativeInstance>({
+      const { forceUpdate } = useForceUpdate();
+      const returnsRef = useRef<NativeInstance>({
         data: initData,
-        setData,
+        setData: ({ visible, style, content, className }: any) => {
+          style = {
+            // @ts-ignore
+            ...initData?.className,
+            // @ts-ignore
+            ...initData?.style,
+            ...className,
+            ...style,
+          };
+          if (style.position === 'fixed') {
+            delete style.position;
+          }
+          if (visible === false) {
+            style.display = 'none';
+          }
+          if (content !== returnsRef.current.data.content) {
+            returnsRef.current.data.content = content;
+            forceUpdate();
+          }
+          setNativePropsRef.current?.({
+            style,
+          });
+        },
       });
-      useImperativeHandle(ref, () => returns, [returns]);
+      const setNativePropsRef = useRef<any>(null);
+      useImperativeHandle(ref, () => returnsRef.current, []);
+      const initRef = useRef(false);
       return (
-        <View onTap={onTap} className={className} style={style as any}>
-          {visible ? children : null}
-          {content}
+        <View
+          onTap={onTap}
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          ref={ref => {
+            setNativePropsRef.current = ref?.view?.current?.setNativeProps;
+            if (!initRef.current && setNativePropsRef.current) {
+              returnsRef.current.setData(initData);
+              initRef.current = true;
+            }
+          }}
+          className={initData?.className}
+          style={initData?.style as any}
+        >
+          {children}
+          {returnsRef.current.data.content}
         </View>
       );
     },
