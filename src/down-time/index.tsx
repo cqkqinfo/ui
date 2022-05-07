fb8dc2a01b4a03a753d61f42426986a998d3e7a2
mushan <mushan0x0@gmail.com>
2022-05-07 10:02:27 +0800

    perf: 优化DownTime性能

--- src/down-time/index.tsx
+++ src/down-time/index.tsx
@@ -3,6 +3,10 @@ import { Native } from '@kqinfo/ui';
 import React, { useEffect, useRef } from 'react';
 import { NativeInstance } from '../native';
 import dayjs from 'dayjs';
+import {
+  setTimeInterval,
+  clearTimeInterval,
+} from 'parsec-hooks/lib/downCountHooks';
 
 interface Props {
   /**
@@ -42,8 +46,8 @@ export default ({ targetDate, format, style, className }: Props) => {
         }),
       });
     fn();
-    const timer = setInterval(fn, 1000);
-    return () => clearInterval(timer);
+    const timer = setTimeInterval(fn, 1000);
+    return () => clearTimeInterval(timer);
   }, [format, targetDate]);
   return <Native ref={ref} initData={{ style, className }} />;
 };
