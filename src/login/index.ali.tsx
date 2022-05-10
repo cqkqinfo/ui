d6901b0685fcd3c0330921a664bc918c3d0b6610
mushan <mushan0x0@gmail.com>
2022-05-10 18:20:03 +0800

    fix: ci

--- src/login/index.ali.tsx
+++ src/login/index.ali.tsx
@@ -2,6 +2,8 @@ import { getAuthCode } from 'remax/ali';
 
 export default async () => {
   const res = await getAuthCode({
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     scopes: ['auth_user', 'hospital_order'],
   });
   return { authCode: res.authCode };
