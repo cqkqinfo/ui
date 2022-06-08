85dbc8b3e9ac9df452d1227dfa53963809def68e
mushan <mushan0x0@gmail.com>
2022-06-08 11:34:35 +0800

    perf: 防止微信native多余的setData
    (cherry picked from commit 2b829a34ee0453e64bf2fd08c96e78f31fe809cf)

--- src/native/index.wechat.tsx
+++ src/native/index.wechat.tsx
@@ -5,56 +5,68 @@ import Native from './native';
 import FlexNative from './flex-native';
 import React, {
   forwardRef,
+  useEffect,
   useImperativeHandle,
   useRef,
-  useState,
 } from 'react';
 import { NativeInstance, Props, Data } from './index';
+import { usePrevious } from 'parsec-hooks';
 
 export default forwardRef<NativeInstance, Props>(
   (
     {
       children,
       flex,
-      initData: { visible = true, className, style, content } = {},
+      initData: {
+        visible = true,
+        className = '',
+        style = '',
+        content = '',
+      } = {},
+      initData,
       onTap,
     },
     ref,
   ) => {
     const dataRef = useRef<Data>({ visible, className, style, content });
-    const [thisRef, setRef] = useState<NativeInstance>({
+    const thisRef = useRef<NativeInstance>({
       data: {},
       setData: data => (dataRef.current = data),
     });
-    useImperativeHandle(ref, () => thisRef, [thisRef]);
+    useImperativeHandle(ref, () => thisRef.current, [thisRef]);
+    const preInitData = usePrevious(initData);
+    useEffect(() => {
+      if (
+        JSON.stringify(preInitData) !== JSON.stringify(initData) &&
+        initData
+      ) {
+        thisRef.current.setData(initData);
+      }
+    }, [initData, preInitData, thisRef]);
+    const preDataStr = useRef(JSON.stringify(dataRef.current));
+    const props = {
+      content,
+      'class-name': className,
+      style,
+      visible,
+      bindthis: ({ detail }: any) => {
+        detail.setData(dataRef.current);
+        thisRef.current = {
+          ...detail,
+          setData: data => {
+            if (preDataStr.current !== JSON.stringify(data)) {
+              detail.setData({ visible, className, style, content, ...data });
+              preDataStr.current = JSON.stringify(data);
+            }
+          },
+        };
+      },
+      bindtap: (e: any) => onTap?.(e.detail),
+    };
     return flex ? (
-      <FlexNative
-        content={content}
-        class-name={className}
-        style={style}
-        visible={visible}
-        bindthis={({ detail }: any) => {
-          detail.setData(dataRef.current);
-          setRef(detail);
-        }}
-        bindtap={(e: any) => onTap?.(e.detail)}
-      >
-        {children}
-      </FlexNative>
+      <FlexNative {...props}>{children}</FlexNative>
     ) : (
-      <Native
-        content={content}
-        class-name={className}
-        style={style}
-        visible={visible}
-        bindtap={(e: any) => onTap?.(e.detail)}
-        bindthis={({ detail }: any) => {
-          detail.setData(dataRef.current);
-          setRef(detail);
-        }}
-      >
-        {children}
-      </Native>
+      <Native {...props}>{children}</Native>
     );
   },
 );
