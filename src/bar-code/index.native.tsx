fac6639dc9a54ad2aae5f21954c9b438b8fc0578
mushan <mushan0x0@gmail.com>
2022-07-01 14:10:40 +0800

    fix: rn 条形码样式

--- src/bar-code/index.native.tsx
+++ src/bar-code/index.native.tsx
@@ -1,26 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Space from '../space';
 import Barcode from 'react-native-barcode-builder';
 import BarCodeProps from './common';
-import useViewLayout from '../use-view-layout';
 export default ({ content, style, ...props }: BarCodeProps) => {
-  const [run, setRun] = React.useState(false);
-  const { width = 0, ...arg } = useViewLayout({ run });
-  useEffect(() => {
-    setRun(!width);
-  }, [width]);
   return (
     <Space
       vertical
       alignItems={'center'}
       style={{ background: '#fff' }}
       {...props}
-      {...arg}
     >
       <Barcode
-        width={(width / content.length) * 1.9}
+        width={1}
         height={style?.height as any}
         value={content}
+        background={'#fff'}
         text={content}
         format="CODE128"
       />
