f1a83dba2d54d8ad125719b8b592ceadfa37cfac
mushan <mushan0x0@gmail.com>
2022-07-01 14:09:37 +0800

    fix: getCurrentPage 微信报错
    (cherry picked from commit d18298238420510f73840e0880be61d103cfe635)

--- src/get-current-page/index.wechat.tsx
+++ src/get-current-page/index.wechat.tsx
@@ -3,7 +3,9 @@ import qs from 'qs';
 export default () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const { route, query } = getCurrentPages()[getCurrentPages().length - 1];
+  const { route, query } = getCurrentPages()[getCurrentPages().length - 1] || {
+    query: {},
+  };
   return `${route}${
     Object.keys(query).length ? `?${qs.stringify(query)}` : ''
   }`;
