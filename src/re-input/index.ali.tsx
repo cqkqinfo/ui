aa9973fbb05e55977b08525d8435e0dc29776e76
mushan <mushan0x0@gmail.com>
2022-07-20 11:32:42 +0800

    feat: reInput支持支付宝小程序
    (cherry picked from commit 85cf6996106dd25f1fc0f255dcb55ee6d18fb627)

---
+++ src/re-input/index.ali.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Input from './native-ali';
+import useInput, { UseInputOption } from './useInput';
+import plainStyle from '@remax/one/esm/useWebPlaceholderStyle/plainStyle';
+
+export type Props = UseInputOption;
+
+export default (props: UseInputOption) => {
+  const {
+    style,
+    placeholderStyle,
+    value,
+    className,
+    placeholder,
+    focus,
+    onBlur,
+    onInput,
+    onFocus,
+    autoHeight,
+    adjustPosition,
+    disableDefaultPadding,
+    cursorSpacing,
+    defaultValue,
+    disabled,
+    maxLength,
+    password,
+    confirmType,
+    confirmHold,
+    showConfirmBar,
+    type,
+    placeholderClassName,
+    delay,
+    onConfirm,
+  } = useInput(props);
+  return (
+    <Input
+      style={plainStyle(style)}
+      value={value}
+      onChange={onInput}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      onConfirm={onConfirm}
+      className={className}
+      delay={delay}
+      placeholder={placeholder}
+      focus={focus}
+      placeholderStyle={plainStyle(placeholderStyle)}
+      autoHeight={autoHeight}
+      adjustPosition={adjustPosition}
+      disableDefaultPadding={disableDefaultPadding}
+      cursorSpacing={cursorSpacing}
+      defaultValue={defaultValue}
+      placeholderClassName={placeholderClassName}
+      disabled={disabled}
+      maxLength={maxLength}
+      password={password}
+      confirmType={confirmType}
+      confirmHold={confirmHold}
+      showConfirmBar={showConfirmBar}
+      type={type}
+    />
+  );
+};
