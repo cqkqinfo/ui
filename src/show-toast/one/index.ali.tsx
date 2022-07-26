d0a92ea28ab329099b98bd68d7372e359330d48a
mushan <mushan0x0@gmail.com>
2022-07-26 09:18:42 +0800

    fix: 支付宝小程序showToast icon默认图标跟别的平台不一致

--- src/show-toast/one/index.ali.tsx
+++ src/show-toast/one/index.ali.tsx
@@ -1,7 +1,7 @@
 import { showToast } from 'remax/ali';
 import { Options } from './index.wechat';
 
-export default ({ title, icon, duration = 3000 }: Options) =>
+export default ({ title, icon = 'success', duration = 3000 }: Options) =>
   new Promise(resolve => {
     showToast({
       type: icon,
