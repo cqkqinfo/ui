4fe56ebe479152e3fd3f9ca714453b527130c6c9
mushan <mushan0x0@gmail.com>
2022-07-27 11:22:58 +0800

    fix: 支付宝小程序showToast时没有隐藏loading
    (cherry picked from commit 7b2570d4aadb4a5a46a9ae7531f6065f6338bc9d)

--- src/show-toast/one/index.ali.tsx
+++ src/show-toast/one/index.ali.tsx
@@ -1,8 +1,9 @@
-import { showToast } from 'remax/ali';
+import { showToast, hideLoading } from 'remax/ali';
 import { Options } from './index.wechat';
 
 export default ({ title, icon = 'success', duration = 3000 }: Options) =>
   new Promise(resolve => {
+    hideLoading();
     showToast({
       type: icon,
       content: title,
