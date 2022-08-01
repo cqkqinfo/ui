3bd7cfdf5a640cb267fdb98831d9a14435fc08b1
mushan <mushan0x0@gmail.com>
2022-08-01 17:53:45 +0800

    feat: perviewImage支持打开base64图片
    (cherry picked from commit c1e0804dcdeb90065ec3f3d58e23c8e5bafc5baa)

--- src/preview-image/index.web.tsx
+++ src/preview-image/index.web.tsx
@@ -6,10 +6,30 @@ import React from 'react';
 import './index.less';
 import { useEffectState } from 'parsec-hooks';
 
+function dataURItoBlob(dataURI: string) {
+  const mimeString = dataURI
+    .split(',')[0]
+    .split(':')[1]
+    .split(';')[0]; // mime类型
+  const byteString = atob(dataURI.split(',')[1]); //base64 解码
+  const arrayBuffer = new ArrayBuffer(byteString.length); //创建缓冲数组
+  const intArray = new Uint8Array(arrayBuffer); //创建视图
+
+  for (let i = 0; i < byteString.length; i++) {
+    intArray[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([intArray], { type: mimeString });
+}
+
 const Component = ({ urls, current, dom }: Options & { dom: HTMLElement }) => {
   const [index, setIndex] = useEffectState(
     current ? urls.findIndex(i => i === current) : 0,
   );
+  urls.forEach((url, index) => {
+    if (!/^(http)/.test(url)) {
+      urls[index] = URL.createObjectURL(dataURItoBlob(url));
+    }
+  });
   return (
     <PhotoSlider
       images={urls.map(item => ({ src: item }))}
