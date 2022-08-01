9417607811aa2e7342ebbd45bffe0c8b2ba9010e
mushan <mushan0x0@gmail.com>
2022-08-01 17:16:24 +0800

    feat: perviewImage支持打开base64图片

--- src/preview-image/index.ali.tsx
+++ src/preview-image/index.ali.tsx
@@ -1,4 +1,22 @@
 import { previewImage } from 'remax/ali';
 
 export type Options = my.IPreviewImageOptions;
-export default (options: Options) => previewImage(options);
+export default async (options: Options) => {
+  options.urls.forEach((url, index) => {
+    if (!/^(http)/.test(url)) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const imgPath = `${my.env.USER_DATA_PATH}/${+new Date()}.png`;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const { writeFileSync } = my.getFileSystemManager();
+      writeFileSync(
+        imgPath,
+        url.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+      );
+      options.urls[index] = imgPath;
+    }
+  });
+  return previewImage(options);
+};
--- src/preview-image/index.tsx
+++ src/preview-image/index.tsx
@@ -1,4 +1,18 @@
-import { previewImage } from 'remax/wechat';
+import { previewImage, getFileSystemManager } from 'remax/wechat';
 
 export type Options = WechatMiniprogram.PreviewImageOption;
-export default (options: Options) => previewImage(options);
+export default async (options: Options) => {
+  options.urls.forEach((url, index) => {
+    if (!/^(http)/.test(url)) {
+      const imgPath = `${wx.env.USER_DATA_PATH}/${+new Date()}.png`;
+      const { writeFileSync } = getFileSystemManager();
+      writeFileSync(
+        imgPath,
+        url.replace(/^data:image\/\w+;base64,/, ''),
+        'base64',
+      );
+      options.urls[index] = imgPath;
+    }
+  });
+  return previewImage(options);
+};
