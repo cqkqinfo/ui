77a6d230303a909f387e31914414b2867b027742
mushan <mushan0x0@gmail.com>
2022-08-02 14:09:01 +0800

    fix: 支付宝小程序预览图片current错误
    (cherry picked from commit c0bdc505f59a48ef13db615f18ca42223bae1d58)

--- src/preview-image/index.ali.tsx
+++ src/preview-image/index.ali.tsx
@@ -2,6 +2,7 @@ import { previewImage } from 'remax/ali';
 
 export type Options = my.IPreviewImageOptions;
 export default async (options: Options) => {
+  options.current = options.urls.findIndex(url => url === options.current + '');
   options.urls.forEach((url, index) => {
     if (!/^(http)/.test(url)) {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
